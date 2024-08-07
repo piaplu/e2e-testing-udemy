7536968a94f272339568f7ae0511e33006bab65a
Pia Müller <pia.mueller@dkv-mobility.com>
2024-08-07 18:39:08 +0200

    Finish Udemy Tutorial section 2 task 21

---
+++ 02-Basics-Project/cypress/e2e/tasks.cy.js
@@ -0,0 +1,14 @@
+describe("tasks management", () => {
+  it("should open and close the new task modal", () => {
+    cy.visit("http://localhost:5173");
+    cy.contains("Add Task").click(); //or: cy.get("button").click();
+    cy.get(".backdrop").click({ force: true });
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
+
+    cy.contains("Add Task").click();
+    cy.contains("Cancel").click();
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
+  });
+});
